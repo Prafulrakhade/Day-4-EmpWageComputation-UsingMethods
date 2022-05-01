8b8e004470217ff579615950a95ec45c25303a0c
Praful <prafulrakhade02@gmail.com>
2022-05-01 12:22:00 +0530

    [praful] Added : Employee is present or not program file

--- EmpWageComputationUsingMethods/Program.cs
+++ EmpWageComputationUsingMethods/Program.cs
@@ -4,10 +4,27 @@ namespace EmpWageComputationUsingMethods
 {
     internal class Program
     {
+        int isPresent = 1;
+        int randomCheck;
+        public void EmpAttendance()
+        {
+            Random random = new Random();
+            randomCheck = random.Next(2);
+            if (randomCheck == isPresent)
+            {
+                Console.WriteLine("The Employee is Present ");
+            }
+            else
+            {
+                Console.WriteLine("The Employee is Absent ");
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("----------------- WELCOME TO THE EMPLOYEE WAGE COMPUTATION IN CLASSES AND METHODS --------------");
             Console.WriteLine();
+            Program prog = new Program();
+            prog.EmpAttendance();
         }
     }
 }
